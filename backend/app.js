b24b06c273fe997b2010e4c94ca324965c3337f3
sarjil patel <sarjilpatel2903@gmail.com>
2022-09-25 21:54:31 -0700

    updated app.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const cookieParser = require("cookie-parser");
 const express = require("express");
 const app = express();
+const path = require("path");
 
 require("dotenv").config({ path: "backend/configs/config.env" });
 
@@ -9,10 +10,6 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("running");
-});
-
 //Importing Routers
 const todoRoutes = require("./routes/todoRoutes");
 const userRoutes = require("./routes/userRoutes");
@@ -20,5 +17,11 @@ const userRoutes = require("./routes/userRoutes");
 //using Routes
 app.use("/api/v1", todoRoutes);
 app.use("/api/v1", userRoutes);
+
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
 
 module.exports = app;
